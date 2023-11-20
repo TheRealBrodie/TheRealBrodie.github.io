a961d33e10bd7641f1f0b84b8ed959b635253dbe
TheRealBrodie <151462720+TheRealBrodie@users.noreply.github.com>
2023-11-20 17:56:52 -0500

    Create Admin.ps1

---
+++ Admin.ps1
@@ -0,0 +1,6 @@
+﻿ Write-Output "Requesting administrator privileges"
+  $User = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
+  $UserName = [Environment]::UserName
+  $Script = $MyInvocation.MyCommand.Path
+  Start-Process cmd.exe -Verb RunAs "-ExecutionPolicy RemoteSigned -File `"$PSCommandPath`" `"${User}`" `"${UserName}`""
+  exit
