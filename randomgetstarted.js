2a63bd808f9304449078731047e1f5e47d2059ea
TheRealBrodie <151462720+TheRealBrodie@users.noreply.github.com>
2024-08-07 16:52:11 -0400

    i just deleted the whole site lmao

---
+++ randomgetstarted.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const ctaButton = document.getElementById('cta-button');
+
+    // Array of random URLs
+    const randomUrls = [
+        'https://TheRealBrodie.github.io/scripts.html',
+        'https://therealbrodie.github.io/Wallpapers.html' 
+    ];
+
+    ctaButton.addEventListener('click', (event) => {
+        event.preventDefault(); // Prevent default link behavior
+        // Get a random URL from the array
+        const randomUrl = randomUrls[Math.floor(Math.random() * randomUrls.length)];
+        // Redirect to the random URL
+        window.location.href = randomUrl;
+    });
+});
+// brodie was here
